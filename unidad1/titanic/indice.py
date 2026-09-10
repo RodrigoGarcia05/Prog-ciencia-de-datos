df34d5d6da2e0fc130b0cb0a8ffed74c035c11e2
Rodrigo García <rodrigoghdz05@gmail.com>
2026-09-10 17:34:02 -0600

    Create indice.py

---
+++ unidad1/titanic/indice.py
@@ -0,0 +1,19 @@
+# ÍNDICE 
+
+- [1. GENERAR UN DATAFRAME CON LOS DATOS DEL FICHERO](#1-generar-un-dataframe-con-los-datos-del-fichero)
+- [2. MOSTRAR POR PANTALLA LAS DIMENSIONES DEL DATAFRAME, EL NÚMERO DE DATOS QUE CONTIENE, LOS NOMBRES DE SUS COLUMNAS Y FILAS, LOS TIPOS DE DATOS DE LAS COLUMNAS, LAS 10 PRIMERAS FILAS Y LAS 10 ÚLTIMAS FILAS](#2-mostrar-por-pantalla-las-dimensiones-del-dataframe-el-numero-de-datos-que-contiene-los-nombres-de-sus-columnas-y-filas-los-tipos-de-datos-de-las-columnas-las-10-primeras-filas-y-las-10-ultimas-filas)
+- [3. MOSTRAR POR PANTALLA LOS DATOS DEL PASAJERO CON IDENTIFICADOR 148](#3-mostrar-por-pantalla-los-datos-del-pasajero-con-identificador-148)
+- [4. MOSTRAR POR PANTALLA LAS FILAS PARES DEL DATAFRAME](#4-mostrar-por-pantalla-las-filas-pares-del-dataframe)
+- [5. MOSTRAR POR PANTALLA LOS NOMBRES DE LAS PERSONAS QUE IBAN EN PRIMERA CLASE ORDENADAS ALFABÉTICAMENTE](#5-mostrar-por-pantalla-los-nombres-de-las-personas-que-iban-en-primera-clase-ordenadas-alfabeticamente)
+- [6. MOSTRAR POR PANTALLA EL PORCENTAJE DE PERSONAS QUE SOBREVIVIERON Y MURIERON](#6-mostrar-por-pantalla-el-porcentaje-de-personas-que-sobrevivieron-y-murieron)
+- [7. MOSTRAR POR PANTALLA EL PORCENTAJE DE PERSONAS QUE SOBREVIVIERON EN CADA CLASE](#7-mostrar-por-pantalla-el-porcentaje-de-personas-que-sobrevivieron-en-cada-clase)
+- [8. ELIMINAR DEL DATAFRAME LOS PASAJEROS CON EDAD DESCONOCIDA](#8-eliminar-del-dataframe-los-pasajeros-con-edad-desconocida)
+- [9. MOSTRAR POR PANTALLA LA EDAD MEDIA DE LAS MUJERES QUE VIAJABAN EN CADA CLASE](#9-mostrar-por-pantalla-la-edad-media-de-las-mujeres-que-viajaban-en-cada-clase)
+- [10. AÑADIR UNA NUEVA COLUMNA BOOLEANA PARA VER SI EL PASAJERO ERA MENOR DE EDAD O NO](#10-anadir-una-nueva-columna-booleana-para-ver-si-el-pasajero-era-menor-de-edad-o-no)
+- [11. MOSTRAR POR PANTALLA EL PORCENTAJE DE MENORES Y MAYORES DE EDAD QUE SOBREVIVIERON EN CADA CLASE](#11-mostrar-por-pantalla-el-porcentaje-de-menores-y-mayores-de-edad-que-sobrevivieron-en-cada-clase)
+- [12. EJERCICIO EXTRA](#12-ejercicio-extra)
+- [13. IMPUTACION](#12-imputacion)
+- [14. TITIANICRODRI](#12-titanicrodri)
+
+
+  ## 1. GENERAR UN DATAFRAME CON LOS DATOS DEL FICHERO
